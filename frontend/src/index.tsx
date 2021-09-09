bb6c46913683ca3492b850012b8b3e46787b77aa
ItamarHavenstein <itamarhavenstein@hotmail.com>
2021-09-09 17:07:12 -0300

    Static charts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'boostrap/package.json';
+import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from './App';
 
